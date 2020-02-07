e5c4a6f9d7b07847803af642f90bb7ad7d44fbbb
Tom Dupree <51969044+tnankie@users.noreply.github.com>
2020-02-06 17:50:32 -0800

    q 10 done
    now back to 9

--- 1520094343_sql_project.sql
+++ 1520094343_sql_project.sql
@@ -107,19 +107,22 @@ WHERE b.starttime LIKE '2012-09-14%'
 HAVING cost > 30
 ORDER BY cost
 -- there we go
+
+
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 -- nfi
 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
-SELECT f.name,
-CASE WHEN b.memid = 0 THEN f.guestcost
-     WHEN b.memid > 0 THEN f.membercost
-     ELSE 0 END AS revenue, sum(2) as total
-FROM `Bookings` b 
-JOIN `Facilities` f ON b.facid = f.facid
-JOIN `Members` m ON b.memid = m.memid
-GROUP BY f.facid
-HAVING total < 1000
-ORDER BY 3
+SELECT name, total
+FROM (SELECT f.name,
+      CASE WHEN b.memid = 0 THEN f.guestcost
+         WHEN b.memid > 0 THEN f.membercost
+         ELSE 0 END AS revenue, sum(2) as total
+      FROM `Bookings` b 
+      JOIN `Facilities` f ON b.facid = f.facid
+      JOIN `Members` m ON b.memid = m.memid
+      GROUP BY f.facid
+      HAVING total < 1000) sub
+ORDER BY 2
